0923cc91fa7fae119990032ed15793f23f8a456a
Jeremy Brown <jeremy@notmessenger.com>
2024-10-26 22:17:39 -0500

    Iterative stream messaging work

--- anthropic/functions/createStreamingMessage.ts
+++ anthropic/functions/createStreamingMessage.ts
@@ -1,4 +1,4 @@
-import { MessageCreateParamsStreaming, MessageStream } from '@anthropic-ai/sdk/resources/messages';
+import { MessageCreateParamsStreaming, MessageStreamEvent, RawMessageStreamEvent } from '@anthropic-ai/sdk/resources/messages';
 import { log } from "@restackio/ai/function";
 import { anthropicClient } from "../utils/client";
 
@@ -7,7 +7,8 @@ import { anthropicClient } from "../utils/client";
  *
  * @see {@link https://docs.anthropic.com/en/api/messages-streaming}
  */
-export async function createStreamingMessage({
+/*
+export async function* createStreamingMessage({
   headers = null,
   body,
   token,
@@ -15,7 +16,7 @@ export async function createStreamingMessage({
   headers?: object | null;
   body: MessageCreateParamsStreaming;
   token?: string;
-  }): Promise<MessageStream> {
+}): AsyncGenerator<RawMessageStreamEvent> {
 
   if (!body) {
     throw new Error(`Missing 'body' value`)
@@ -24,7 +25,41 @@ export async function createStreamingMessage({
   try {
     const anthropic = anthropicClient({ token });
 
-    return anthropic.messages.stream(body, headers)
+    const stream = await anthropic.messages.stream(body, headers);
+
+    for await (const event of stream) {
+      yield event;
+    }
+
+  } catch (error) {
+    log.error("Anthropic Create Streaming Message error", { error });
+    throw new Error(`Anthropic Create Streaming Message error ${error}`);
+  }
+}
+*/
+
+export async function* createStreamingMessage({
+  headers = null,
+  body,
+  token,
+}: {
+  headers?: object | null;
+  body: MessageCreateParamsStreaming;
+  token?: string;
+}): AsyncGenerator<MessageStreamEvent> {
+
+  if (!body) {
+    throw new Error(`Missing 'body' value`)
+  }
+
+  try {
+    const anthropic = anthropicClient({ token });
+
+    const stream = anthropic.messages.stream(body, headers);
+
+    for await (const event of stream) {
+      yield event;
+    }
 
   } catch (error) {
     log.error("Anthropic Create Streaming Message error", { error });
